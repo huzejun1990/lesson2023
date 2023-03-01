2eb4013ee0ced586c5c769c05d890768955c22cc
huzejun <huzejun1990>
2023-03-02 04:19:28 +0800

    day001_machine_learing.py 最终更新

--- mechine_learning2023/day001_machine_learing.py
+++ mechine_learning2023/day001_machine_learing.py
@@ -2,6 +2,7 @@
 # @Time : 2023/3/2 3:21
 
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 def datasets_demo():
 
@@ -11,6 +12,9 @@ def datasets_demo():
     print("查看特征值的名字:\n",iris.feature_names)
     print("查看特征值:\n",iris.data,iris.data.shape)
 
+    # 数据集划分
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=22)
+    print("训练集的特征值:\n",x_train,x_train.shape)
     return None
 
 if __name__ == "__main__":
